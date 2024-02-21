8f377f366aa1bdb0b28cc4917017f525d76144cc
Jorge Peraza <j.peraza.22@gmail.com>
2024-02-20 22:26:19 -0700

    initial commit with partially complete part 1. working on styling header

---
+++ src/components/homeComponents/HomeScreen.jsx
@@ -0,0 +1,30 @@
+import { React, useEffect } from 'react'
+import AdBanner from './AdBanner'
+import axios from "axios";
+
+const HomeScreen = () => {  
+
+  const getRecipes = () => {
+    axios
+      .get(
+        `https://recipes.devmountain.com/recipes`
+      )
+      .then((res) => {
+        // setRecipes(res.data)
+        console.log(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return (
+    <div>
+      <AdBanner />
+      {/* Much code from Part 2 will be placed around here. Do your best! */}
+    </div>
+  )
+}
+
+export default HomeScreen
