2eff1230c04ec8f439c249e70cee5aace8fbe310
Jorge Peraza <j.peraza.22@gmail.com>
2024-02-27 21:57:33 -0700

    Adds finished part 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link }  from "react-router-dom"
-import "./Banners.css"
+import styles from "./Banners.module.css"
 
 const Header = () => {
   return (
@@ -8,7 +8,7 @@ const Header = () => {
       <h2>Devmountain Eatery</h2>
       <nav >
         <Link to="/" >
-          <button className="button">Home</button>
+          <button className={styles.button}>Home</button>
         </Link>
         <Link to="/newRecipe">
           <button>Add Recipe</button>
